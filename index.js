065e4e20bc327b3b07e60c80950afe597300e140
louis baptiste <louisvis@ymail.com>
2020-03-07 06:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 // Code your solution in this file.
-function lowerCaseDrivers(array, callback) {
-    const newArr = [];
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
+  });
+}
 
-    for (const element of array) {
-        newArr.push(callback(element));
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (driver) {
+      return driver.name;
+    });
+}
+
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
     }
 
-    return newArr;
+    return matches;
+  });
+}
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function (driver) {
+      return driver.name;
+    });
 }
