527135a36c6d894d9e5aa3fc10f47487e8b60c77
louis baptiste <louisvis@ymail.com>
2020-03-07 06:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array, callback) {
+    const newArr = [];
+
+    for (const element of array) {
+        newArr.push(callback(element));
+    }
+
+    return newArr;
+}
